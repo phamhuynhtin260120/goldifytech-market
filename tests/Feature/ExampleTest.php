edac0db88e196e362c725852355c1af2dd196d5a
PhamHuynhTin <phamhuynhtin260120@gmail.com>
2026-07-03 21:22:02 +0700

    .....

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,13 @@
 <?php
 
+use Inertia\Testing\AssertableInertia as Assert;
+
 test('returns a successful response', function () {
     $response = $this->get(route('home'));
 
-    $response->assertOk();
+    $response
+        ->assertOk()
+        ->assertInertia(fn (Assert $page) => $page
+            ->component('welcome')
+        );
 });
